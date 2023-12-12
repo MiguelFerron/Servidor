ca9c552ce4043657ecd69ac93c022cfc6df830f2
miguel ferron <miguelfv2606@gmail.com>
2023-12-12 10:35:13 +0100

    aa

--- DWES05/web/smarty/tarea/templates_c/a6e03cafa295417c14e1aa089d947fa2e6d327b3_0.file.ordenador.tpl.php
+++ DWES05/web/smarty/tarea/templates_c/a6e03cafa295417c14e1aa089d947fa2e6d327b3_0.file.ordenador.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.4, created on 2023-12-03 11:17:56
+/* Smarty version 4.3.4, created on 2023-12-06 13:11:20
   from 'C:\xampp\htdocs\Servidor\DWES05\web\smarty\tarea\templates\ordenador.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.4',
-  'unifunc' => 'content_656c55d4315d44_87145366',
+  'unifunc' => 'content_657064e8936a53_31108043',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'a6e03cafa295417c14e1aa089d947fa2e6d327b3' => 
     array (
       0 => 'C:\\xampp\\htdocs\\Servidor\\DWES05\\web\\smarty\\tarea\\templates\\ordenador.tpl',
-      1 => 1701597991,
+      1 => 1701599516,
       2 => 'file',
     ),
   ),
@@ -20,47 +20,59 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_656c55d4315d44_87145366 (Smarty_Internal_Template $_smarty_tpl) {
-?><!-- ordenador.tpl -->
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getnombrecorto();?>
+function content_657064e8936a53_31108043 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!-- Desarrollo Web en Entorno Servidor -->
+<!-- Tarea 5 : Programación orientada a objetos en PHP -->
+<!-- Tienda Web: ordenador.php -->
+<!-- Nueva plantilla para presentar los datos de los ordenadores -->
+<html>
+	<head>
+	  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	  <title>Ordenador <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getcodigo();?>
 </title>
-    <!-- Agrega tus estilos CSS aquí -->
-</head>
-<body>
-    <h1><?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getnombrecorto();?>
+	  <link href="tienda.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body class="pagproductos">
+
+		<div id="contenedor" style="background-color: #df8;">
+		  <div id="encabezado">
+		    <h1><?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getnombrecorto();?>
 </h1>
-
-    <ul>
-        <li><strong>Código:</strong> <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getcodigo();?>
-</li>
-        <li><strong>Procesador:</strong> <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getprocesador();?>
-</li>
-        <li><strong>RAM:</strong> <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getram();?>
-</li>
-        <li><strong>Disco:</strong> <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getdisco();?>
-</li>
-        <li><strong>Tarjeta Gráfica:</strong> <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getgrafica();?>
-</li>
-        <li><strong>Unidad Óptica:</strong> <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getunidadoptica();?>
-</li>
-        <li><strong>Sistema Operativo:</strong> <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getso();?>
-</li>
-        <li><strong>Otros:</strong> <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getotros();?>
-</li>
-        <li><strong>PVP:</strong> <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getpvp();?>
- euros</li>
-        <li><strong>Descripción:</strong> <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getdescripcion();?>
-</li>
-    </ul>
-
-    <!-- Agrega más contenido según sea necesario -->
-
-</body>
-</html>
-<?php }
+				<p><b>C&oacute;digo: <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getcodigo();?>
+</b></p>
+		  </div>
+		  <div class="ordenador">
+				<h2>Caracter&iacute;sticas:</h2>
+				<p><b>Procesador:</b> <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getprocesador();?>
+</p>
+				<p><b>RAM:</b> <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getram();?>
+ GB</p>
+				<p><b>Tarjeta gr&aacute;fica:</b> <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getgrafica();?>
+</p>
+				<p><b>Unidad &oacute;ptica:</b> <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getunidadoptica();?>
+</p>
+				<p><b>Sistema Operativo:</b> <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getso();?>
+</p>		
+				<p><b>Otros:</b> <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getotros();?>
+</p>
+				<p><b>PVP:</b> <?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getpvp();?>
+</p>
+				<h2>Descripci&oacute;n:</h2>
+				<p><?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getdescripcion();?>
+</p>
+			</div>
+  
+		  <div id="pie" style="height: 25px">
+				<form action="productos.php" method="post">
+					<input type='submit' name='' value='Volver' class='izq boton' />
+					<input type='hidden' name='cod' value='<?php echo $_smarty_tpl->tpl_vars['ordenador']->value->getcodigo();?>
+' />
+					<input type='submit' name='enviar' value='Comprar' class='dch boton' />
+				</form>
+		  </div>
+		</div>
+	</body>
+</html><?php }
 }
