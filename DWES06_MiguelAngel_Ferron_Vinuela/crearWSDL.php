88fedc008fa8f0acd38be318f98d0dfabb349e65
miguel ferron <miguelfv2606@gmail.com>
2023-12-19 10:33:21 +0100

    a

--- DWES06_MiguelAngel_Ferron_Vinuela/crearWSDL.php
+++ DWES06_MiguelAngel_Ferron_Vinuela/crearWSDL.php
@@ -1,11 +1,11 @@
 <?php
-require_once 'Funciones.php';
-require_once 'WSDLDocument.php';
+        require_once 'Funciones.php';
+        require_once 'WSDLDocument.php';
 
-$wsdl = new WSDLDocument(
-        'Funciones',
-        'http://127.0.0.1/servidor/DWES06_MiguelAngel_Ferron_Vinuela/servicio.php',
-        'http://127.0.0.1/servidor/DWES06_MiguelAngel_Ferron_Vinuela');
-header ('Content-Type: text/xml');
-echo $wsdl->saveXML();
+        $wsdl = new WSDLDocument(
+                'Funciones',
+                'http://127.0.0.1/servidor/DWES06_MiguelAngel_Ferron_Vinuela/servicio.php',
+                'http://127.0.0.1/servidor/DWES06_MiguelAngel_Ferron_Vinuela');
+        header ('Content-Type: text/xml');
+        echo $wsdl->saveXML();
 ?>
