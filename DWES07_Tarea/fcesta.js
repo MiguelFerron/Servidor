57649deb2f94fc176e9287f754c38987fb07c6d5
miguel ferron <miguelfv2606@gmail.com>
2023-12-25 11:46:32 +0100

    a

--- DWES07_Tarea/fcesta.js
+++ DWES07_Tarea/fcesta.js
@@ -1,27 +1,24 @@
 // función para añadir productos a la cesta
-function anadeCesta(){
-	// recogemos el valor introducido en el campo 'cod' del formulario
-	var producto = document.getElementById("cod").value;
-	// recuperamos el número aleatorio de esta sesión
-	var micod=document.getElementById("numAle").value;
-	// llamamos a la función php 'anadirCesta' pasándole el valor recogido anteriormente y la sesión actual
-	var respuesta = xajax.request({xjxfun:"anadirCesta"}, {mode:"synchronous", parameters: [producto,micod]});
-	// devolvemos el valor obtenido por la función anterior
-	return respuesta;
+function anadeCesta() {
+    // recogemos el valor introducido en el campo 'cod' del formulario
+    let producto = document.getElementById("cod").value;
+    // recuperamos el número aleatorio de esta sesión
+    let micod = document.getElementById("numAle").value;
+    // llamamos a la función php 'anadirCesta' pasándole el valor recogido anteriormente y la sesión actual
+    xajax.request({ xjxfun: "anadirCesta" }, { mode: "asynchronous", parameters: [producto, micod] });
+    // puedes manejar la respuesta o realizar acciones adicionales si es necesario
 }
 
 // función para vaciar la cesta
-function vaciaCesta(){
-	// llamamos a la función php 'vaciarCesta' sin enviarle ningún parámetro
-	var respuesta = xajax.request({xjxfun:"vaciarCesta"}, {mode:"synchronous"});
-	// devolvemos la palabra 'vaciar' para indicar que se ha ejecutado ese proceso
-	return "vaciar";
+function vaciaCesta() {
+    // llamamos a la función php 'vaciarCesta' sin enviarle ningún parámetro
+    xajax.request({ xjxfun: "vaciarCesta" }, { mode: "asynchronous" });
+    // puedes manejar la respuesta o realizar acciones adicionales si es necesario
 }
 
 // función que nos muestra los artículos existentes en la cesta
-function muestraCesta(){
-	// llamamos a la función php 'mostrarCesta' sin enviarle ningún parámetro
-	var respuesta = xajax.request({xjxfun:"mostrarCesta"}, {mode:"synchronous"});
-	// devolvemos la palabra 'comprar' para indicar que se ha ejecutado ese proceso
-	return "comprar";
+function muestraCesta() {
+    // llamamos a la función php 'mostrarCesta' sin enviarle ningún parámetro
+    xajax.request({ xjxfun: "mostrarCesta" }, { mode: "asynchronous" });
+    // puedes manejar la respuesta o realizar acciones adicionales si es necesario
 }
